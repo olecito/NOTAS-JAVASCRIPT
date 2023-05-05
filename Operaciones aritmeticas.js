7d2efc1abf43182d6a993bf031f7924e8dfe0368
olecito <olezurita@icloud.com>
2023-05-05 19:16:18 +0200

    se agregan notas de operaciones aritmeticas basicas

---
+++ Operaciones aritmeticas.js
@@ -0,0 +1,68 @@
+
+//SUMA
+let suma = 7 + 12; 
+
+//RESTA
+let resta = 12 - 7; // resultado positivo 
+ resta = 7 - 12; // resultado negativo 
+
+// MULTIPLICACION
+let producto = 5 * 3;
+producto = 9 * 0;
+
+// DIVISION
+
+let cociente = 20 / 2; 
+cociente = 17 / 31; 
+
+
+// se pueden hacer operaciones aritmeticas con valores decimales, el decimal lo representamos en ingles, es decir, con un punto ( . ) 
+
+// si dividimos entre cero la consola va a devolver infinito  
+
+
+// RESTO DE UNA DIVISION  
+
+let resto = 15 % 5; //  Retorna el restante después de dividir el número de la izquierda en porciones enteras del de la derecha.
+resto = 8 % 3; // 8 % 3 (retorna 2, como tres está dos veces en 8, quedando 2 restantes.)
+
+
+// INCREMENTAR O DISMINUIR EL VALOR DE UNA VARIABLE 
+
+let librosComprados = 105; 
+let librosComprados1 = 105; 
+
+//opcion 1
+
+librosComprados = librosComprados + 1; // 
+librosComprados1 = librosComprados1 - 1;
+
+
+//opcion 2
+
+librosComprados++ // esto incrementa el valor de una variable de uno en uno 
+librosComprados1-- // esto disminuye el valor de una variable de uno en uno 
+
+// ASIGNACION DE VALORES
+
+let a = 23; 
+
+a += 5; // Suma 5 al valor actual de a y lo agrega a esa variable (+=)
+
+let totalVentas = 13567.34;
+totalVentas += 345.67; 
+
+a -= 5; // Resta 5 al valor actual de a y lo resta de a esa variable (-=)
+
+let totalDeudas = 13567.34;
+totalDeudas -= 345.67; 
+
+a *= 5; // Multiplica por 5 al valor actual de a y lo agrega a esa variable (*=)
+
+let totalSalario = 13567.34;
+totalVentas *= 345.67; 
+
+a /= 5; // divide entre 5 al valor actual de a y lo agrega a esa variable (*=)
+
+let totalD = 13567.34;
+totalD /= 345.67; 
