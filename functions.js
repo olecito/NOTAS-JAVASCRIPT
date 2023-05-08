b055741fa73d2517485c2387480778aa3781e93e
olecito <olezurita@icloud.com>
2023-05-08 18:20:59 +0200

    practicas 2

--- functions.js
+++ functions.js
@@ -26,4 +26,23 @@ function canIVote(age) {
      console.log(canIVote(19)) // Should print true
     
     // We encourage you to add more function calls of your own to test your code!
+
+    // Write your function here:
+const agreeOrDisagree = (first, second) => {
+    if (first === second) {
+      return 'You agree!'
+    } else {
+      return 'You disagree!'
+    }
+  }
+  
+  
+  
+  
+  
+  // Uncomment the line below when you're ready to try out your function
+   console.log(agreeOrDisagree("yep", "yep")) 
+  // Should print 'You agree!'
+  
+  // We encourage you to add more function call of your own to test your code!
     
