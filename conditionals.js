ed456ca3d3ba8051af082dc4d75d8f61dcc9fe45
olecito <olezurita@icloud.com>
2023-05-09 10:39:28 +0200

    practica 4

--- conditionals.js
+++ conditionals.js
@@ -54,3 +54,15 @@ const finalGrade = (midterm, final, homework) => {
      console.log(finalGrade(99, 92, 95)) // Should print 'A'
     
     // We encourage you to add more function calls of your own to test your code!
+
+    // Write your function here:
+
+const reportingForDuty = (rank, lastName) =>
+{ return `${rank} ${lastName} reporting for duty!`
+}
+
+
+// Uncomment the line below when you're ready to try out your function
+console.log(reportingForDuty('Private', 'Fido')) // Should return 'Private Fido reporting for duty!'
+
+// We encourage you to add more function calls of your own to test your code!
