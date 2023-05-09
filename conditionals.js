340c9da398ef3362d1f245e76ba620712fa0a1a8
olecito <olezurita@icloud.com>
2023-05-09 10:55:03 +0200

    practicas

--- conditionals.js
+++ conditionals.js
@@ -77,3 +77,23 @@ const rollTheDice = () => {
   }
   
   console.log(rollTheDice());
+
+  const calculateWeight = (earthWeight, planet) => {
+    switch (planet) {
+      case 'Mercury':
+      return earthWeight * 0.378;
+      case 'Venus':
+      return earthWeight * 0.907;
+      case 'Mars':
+      return earthWeight * 0.377;
+      case 'Jupiter':
+      return earthWeight * 2.36;
+      case 'Saturn':
+      return earthWeight * 0.916;
+      default:
+      return 'Invalid Planet Entry. Try: Mercury, Venus, Mars, Jupiter, or Saturn.'
+    }
+  }
+  
+  
+  
