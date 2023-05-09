b521edcadea8172db29b9ce160fb54d034dbc453
olecito <olezurita@icloud.com>
2023-05-09 10:44:45 +0200

    practicas

--- conditionals.js
+++ conditionals.js
@@ -66,3 +66,14 @@ const reportingForDuty = (rank, lastName) =>
 console.log(reportingForDuty('Private', 'Fido')) // Should return 'Private Fido reporting for duty!'
 
 // We encourage you to add more function calls of your own to test your code!
+
+const rollTheDice = () => {
+    // Math.random() gives us a random number from 0 up to, but not including, 1
+    // We multiplied that by 6 to get a number between 0 and up to, but not including, 6
+    // But since we actually wanted numbers from 1 to 6, inclusive, we added 1
+      let die1 = Math.floor(Math.random() * 6 + 1)
+      let die2 = Math.floor(Math.random() * 6 + 1)
+      return die1 + die2
+  }
+  
+  console.log(rollTheDice());
